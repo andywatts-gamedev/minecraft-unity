7ca833be035c06ebc4477bab72ab7d370d2b3d7e
Andrew Watts-Curnow <andywatts@yahoo.com>
2023-10-19 21:29:09 -0400

    meshtest is good

--- Assets/Scripts/MeshTest.cs
+++ Assets/Scripts/MeshTest.cs
@@ -11,21 +11,9 @@ public class MeshTest : MonoBehaviour
 {
     public int numFaces;
             
-    public struct MyVertex
-    {
-        public float3 Position;
-        public float3 Normal;
-        public half4 Tangent;
-    }
-
-    public struct MyVertex2
-    {
-        public half2 TexCoord0;
-        public Color32 Color;
-    }
-
     private void Start()
     {
+        // Define MeshData
         var meshDataArray = Mesh.AllocateWritableMeshData(1);
         var meshData = meshDataArray[0];
         meshData.SetVertexBufferParams(numFaces * 4, new[]
@@ -33,29 +21,47 @@ public class MeshTest : MonoBehaviour
             new VertexAttributeDescriptor( VertexAttribute.Position, dimension: 3, stream: 0),
             new VertexAttributeDescriptor( VertexAttribute.Normal, dimension: 3, stream: 0 ),
             new VertexAttributeDescriptor( VertexAttribute.Tangent, VertexAttributeFormat.Float16, dimension: 4, stream: 0 ),
-            new VertexAttributeDescriptor( VertexAttribute.TexCoord0, dimension: 2, stream: 1 ),
-            new VertexAttributeDescriptor( VertexAttribute.Color, dimension: 4, stream: 1 ),
+            new VertexAttributeDescriptor( VertexAttribute.TexCoord0, VertexAttributeFormat.Float16, dimension: 2, stream: 1 ),
+            new VertexAttributeDescriptor( VertexAttribute.Color, VertexAttributeFormat.UNorm8, dimension: 4, stream: 2 ),
         });
-        
-        
-        
-        var vertices = meshData.GetVertexData<MyVertex>();
         meshData.SetIndexBufferParams(numFaces * 6, IndexFormat.UInt16);
+        
+        
+        
+        // Populate MeshData
+        var vertexStream0 = meshData.GetVertexData<VertexStream0>(0);
         var triangles = meshData.GetIndexData<ushort>();
+        var texCoords = meshData.GetVertexData<half2>(1);
+        var colors = meshData.GetVertexData<Color32>(2);
         
         for (var i = 0; i < numFaces; i++)
         {
+            // VertexStream0: Position, Normal, Tangent
             var voxelXyz = new float3( Random.Range(0, 16), 1, Random.Range(0, 16));
-            vertices[i*4 + 0] = new MyVertex {Position = new float3(0f, 1f, 0f) + voxelXyz};
-            vertices[i*4 + 1] = new MyVertex {Position = new float3(0f, 1f, 1f) + voxelXyz};
-            vertices[i*4 + 2] = new MyVertex {Position = new float3(1f, 1f, 1f) + voxelXyz};
-            vertices[i*4 + 3] = new MyVertex {Position = new float3(1f, 1f, 0f) + voxelXyz};
+            vertexStream0[i*4 + 0] = new VertexStream0 {Position = new float3(0f, 1f, 0f) + voxelXyz};
+            vertexStream0[i*4 + 1] = new VertexStream0 {Position = new float3(0f, 1f, 1f) + voxelXyz};
+            vertexStream0[i*4 + 2] = new VertexStream0 {Position = new float3(1f, 1f, 1f) + voxelXyz};
+            vertexStream0[i*4 + 3] = new VertexStream0 {Position = new float3(1f, 1f, 0f) + voxelXyz};
+            
+            // Triangles
             triangles[i*6 + 0] = (ushort)(i*4 + 0);
             triangles[i*6 + 1] = (ushort)(i*4 + 1);
             triangles[i*6 + 2] = (ushort)(i*4 + 3);
             triangles[i*6 + 3] = (ushort)(i*4 + 1);
             triangles[i*6 + 4] = (ushort)(i*4 + 2);
             triangles[i*6 + 5] = (ushort)(i*4 + 3);
+
+            // Texcoords
+            texCoords[i*4 + 0] = (half)0;
+            texCoords[i*4 + 1] = half2((half)0, (half)1);
+            texCoords[i*4 + 2] = (half)1;
+            texCoords[i*4 + 3] = half2((half)1, (half)0);
+            
+            // Colors
+            colors[i * 4 + 0] = new Color32(1, 1, 1, 1);
+            colors[i * 4 + 1] = new Color32(1, 1, 1, 1);
+            colors[i * 4 + 2] = new Color32(1, 1, 1, 1);
+            colors[i * 4 + 3] = new Color32(1, 1, 1, 1); 
         }
 
         meshData.subMeshCount = 1;
@@ -67,6 +73,17 @@ public class MeshTest : MonoBehaviour
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
         GetComponent<MeshFilter>().mesh = mesh;
+        
+        
+        GetComponent<MeshRenderer>().materials[0].SetTexture("_TextureArray", Blocks.Instance.opaqueTexture2DArray);
+    }
+
+    
+    public struct VertexStream0
+    {
+        public float3 Position;
+        public float3 Normal;
+        public half4 Tangent;
     }
 
 }

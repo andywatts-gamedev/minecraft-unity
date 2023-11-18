58716ada4bba1624175a9267ae0765687b019212
Andrew Watts-Curnow <andywatts@yahoo.com>
2023-11-18 12:05:21 -0500

    Toolbar

--- Assets/Scripts/Blocks/Block.cs
+++ Assets/Scripts/Blocks/Block.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "Block", menuName = "Oasis/Block")]
@@ -12,6 +14,125 @@ public class Block : ScriptableObject
     public BlockType Type;
     public SideTexture[] SideTextures;
     public ModelElement[] ModelElements;
+    public Texture2D image;
+
+    public Mesh GenerateMesh()
+    {
+        var numFaces = 6;
+        var meshDataArray = Mesh.AllocateWritableMeshData(1);
+        var meshData = meshDataArray[0];
+        meshData.SetVertexBufferParams(numFaces * 4, new[]
+        {
+            new VertexAttributeDescriptor(VertexAttribute.Position, dimension: 3, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.Normal, dimension: 3, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.Tangent, VertexAttributeFormat.Float16, dimension: 4, stream: 0),
+            new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float16, dimension: 2, stream: 1),
+            new VertexAttributeDescriptor(VertexAttribute.Color, VertexAttributeFormat.UNorm8, dimension: 4, stream: 2)
+        });
+        meshData.SetIndexBufferParams(numFaces * 6, IndexFormat.UInt16);
+
+        // VERTICES
+        var vertexStream0 = meshData.GetVertexData<VertexStream0>(0);
+        var offset = new float3(0.5f, 0.5f, 0.5f);
+        
+        // East
+        vertexStream0[0 * 4 + 0] = new VertexStream0 {Position = new float3(1f, 0f, 0f) - offset };
+        vertexStream0[0 * 4 + 1] = new VertexStream0 {Position = new float3(1f, 1f, 0f) - offset };
+        vertexStream0[0 * 4 + 2] = new VertexStream0 {Position = new float3(1f, 1f, 1f) - offset };
+        vertexStream0[0 * 4 + 3] = new VertexStream0 {Position = new float3(1f, 0f, 1f) - offset };
+        
+        // Up
+        vertexStream0[1 * 4 + 0] = new VertexStream0 {Position = new float3(0f, 1f, 0f) - offset };
+        vertexStream0[1 * 4 + 1] = new VertexStream0 {Position = new float3(0f, 1f, 1f) - offset };
+        vertexStream0[1 * 4 + 2] = new VertexStream0 {Position = new float3(1f, 1f, 1f) - offset };
+        vertexStream0[1 * 4 + 3] = new VertexStream0 {Position = new float3(1f, 1f, 0f) - offset };
+        
+        // North
+        vertexStream0[2 * 4 + 0] = new VertexStream0 {Position = new float3(1f, 0f, 1f) - offset };
+        vertexStream0[2 * 4 + 1] = new VertexStream0 {Position = new float3(1f, 1f, 1f) - offset };
+        vertexStream0[2 * 4 + 2] = new VertexStream0 {Position = new float3(0f, 1f, 1f) - offset };
+        vertexStream0[2 * 4 + 3] = new VertexStream0 {Position = new float3(0f, 0f, 1f) - offset };
+        
+        // West
+        vertexStream0[3 * 4 + 0] = new VertexStream0 {Position = new float3(0f, 0f, 1f) - offset };
+        vertexStream0[3 * 4 + 1] = new VertexStream0 {Position = new float3(0f, 1f, 1f) - offset };
+        vertexStream0[3 * 4 + 2] = new VertexStream0 {Position = new float3(0f, 1f, 0f) - offset };
+        vertexStream0[3 * 4 + 3] = new VertexStream0 {Position = new float3(0f, 0f, 0f) - offset };
+        
+        // Down
+        vertexStream0[4 * 4 + 0] = new VertexStream0 {Position = new float3(0f, 0f, 0f) - offset };
+        vertexStream0[4 * 4 + 1] = new VertexStream0 {Position = new float3(1f, 0f, 0f) - offset };
+        vertexStream0[4 * 4 + 2] = new VertexStream0 {Position = new float3(1f, 0f, 1f) - offset };
+        vertexStream0[4 * 4 + 3] = new VertexStream0 {Position = new float3(0f, 0f, 1f) - offset };
+        
+        // South
+        vertexStream0[5 * 4 + 0] = new VertexStream0 {Position = new float3(0f, 0f, 0f) - offset };
+        vertexStream0[5 * 4 + 1] = new VertexStream0 {Position = new float3(0f, 1f, 0f) - offset };
+        vertexStream0[5 * 4 + 2] = new VertexStream0 {Position = new float3(1f, 1f, 0f) - offset };
+        vertexStream0[5 * 4 + 3] = new VertexStream0 {Position = new float3(1f, 0f, 0f) - offset };
+        
+        
+        
+        // Triangles
+        var triangles = meshData.GetIndexData<ushort>();
+        var texCoords = meshData.GetVertexData<half2>(1);
+        var colors = meshData.GetVertexData<Color32>(2);
+        var opaqueTriangles = new NativeList<ushort>(Allocator.Temp);
+        var transparentTriangles = new NativeList<ushort>(Allocator.Temp);
+        var alphaClipTriangles = new NativeList<ushort>(Allocator.Temp);
+        
+        NativeList<ushort> tris;
+        if (Type == BlockType.AlphaClip)
+            tris = alphaClipTriangles;
+        else if (Type == BlockType.Transparent)
+            tris = transparentTriangles;
+        else 
+            tris = opaqueTriangles;
+        
+        for (var i = 0; i < 6; i++)
+        {
+            // Tris
+            tris.Add((ushort)(i*4 + 0));
+            tris.Add((ushort)(i*4 + 1));
+            tris.Add((ushort)(i*4 + 2));
+            tris.Add((ushort)(i*4 + 2));
+            tris.Add((ushort)(i*4 + 3));
+            tris.Add((ushort)(i*4 + 0));
+            
+            // UVs
+            texCoords[i*4 + 0] = (half)0;
+            texCoords[i*4 + 1] = new half2((half)0, (half)1);
+            texCoords[i*4 + 2] = (half)1;
+            texCoords[i*4 + 3] = new half2((half)1, (half)0);
+
+            // TextureIndex
+            var textureIndex = (byte) SideTextures[i].TextureObject.TextureIndex;
+            colors[i*4 + 0] = new Color32(0, 0, 0,textureIndex);
+            colors[i*4 + 1] = new Color32(0, 0, 0,textureIndex);
+            colors[i*4 + 2] = new Color32(0, 0, 0,textureIndex);
+            colors[i*4 + 3] = new Color32(0, 0, 0,textureIndex); 
+        }
+        
+        for (var i = 0; i < opaqueTriangles.Length; i++)
+            triangles[i] = opaqueTriangles[i];
+        for (var i = 0; i < alphaClipTriangles.Length; i++)
+            triangles[opaqueTriangles.Length +  i] = alphaClipTriangles[i];
+        for (var i = 0; i < transparentTriangles.Length; i++)
+            triangles[opaqueTriangles.Length + alphaClipTriangles.Length + i] = transparentTriangles[i];
+
+        
+        meshData.subMeshCount = 3;
+        meshData.SetSubMesh(0, new SubMeshDescriptor(0, opaqueTriangles.Length));
+        meshData.SetSubMesh(1, new SubMeshDescriptor(opaqueTriangles.Length, alphaClipTriangles.Length));
+        meshData.SetSubMesh(2, new SubMeshDescriptor(opaqueTriangles.Length + alphaClipTriangles.Length, transparentTriangles.Length));
+
+        var mesh = new Mesh();
+        mesh.name = name;
+        Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        return mesh;
+    }
 }
 
 
--- Assets/UI/Hud/Toolbar.cs
+++ Assets/UI/Hud/Toolbar.cs
@@ -1,59 +1,72 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UIElements;
-// using UnityEngine.Serialization;
-// using Texture = UnityEngine.Texture;
-//
-// public class Toolbar : MonoBehaviour
-// {
-//     private static Toolbar _instance;
-//     public static Toolbar Instance => _instance;
-//
-//     public DomainName[] domainNames;
-//
-//     private VisualElement root;
-//     private VisualElement toolbar;
-//     private UQueryBuilder<VisualElement> slots;
-//     public int selectedItem;
-//     
-//
-//     void Awake()
-//     {
-//         _instance = this;
-//     }
-//     
-//     void Start()
-//     {
-//         root = gameObject.GetComponent<UIDocument>().rootVisualElement;
-//         toolbar = root.Q<VisualElement>("toolbar");
-//         slots = toolbar.Query<VisualElement>("toolbarSlot");
-//         domainNames = new Oasis.Grpc.DomainName[10];
-//         SetActiveToolbarItem(0);
-//     }
-//
-//     public void SetToolbarItem(int slotIndex, DomainName domainName, Texture blockImage)
-//     {
-//         slotIndex = slotIndex - 1;
-//         if (slotIndex == -1)
-//             slotIndex = 9;
-//         
-//         domainNames[slotIndex] = domainName;
-//         
-//         var slot = slots.AtIndex(slotIndex);
-//         var image = new Image();
-//         image.image = blockImage;
-//         if (slot.childCount > 0)
-//             slot.RemoveAt(0);
-//         slot.Add(image);
-//     }
-//
-//     public void SetActiveToolbarItem(int slotIndex)
-//     {
-//         selectedItem = slotIndex;
-//         for (int i = 0; i < 10; i++)
-//             slots.AtIndex(i).RemoveFromClassList("toolbarSlotActive");
-//         slots.AtIndex(slotIndex).AddToClassList("toolbarSlotActive");
-//     }
-// }
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Toolbar : MonoBehaviour
+{
+    public int selectedItem;
+    public List<Block> blocks;
+    public GameObject blockPrefab;
+    private VisualElement root;
+    private UQueryBuilder<VisualElement> slots;
+    private VisualElement toolbar;
+    public static Toolbar Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        root = gameObject.GetComponent<UIDocument>().rootVisualElement;
+        toolbar = root.Q<VisualElement>("toolbar");
+        slots = toolbar.Query<VisualElement>("toolbarSlot");
+
+        var blockGO = Instantiate(blockPrefab);
+        blockGO.transform.position = new Vector3(-0.0f, -0.2f, -0f);
+        blockGO.transform.rotation = Quaternion.Euler(15f, 180+47, 15f);
+        var meshFilter = blockGO.GetComponent<MeshFilter>();
+        blockGO.GetComponent<MeshRenderer>().materials[0].SetTexture("_TextureArray", Textures.Instance.opaqueTexture2DArray);
+
+        foreach (var (block, slotIndex) in blocks.Select((value, i) => (value, i)))
+        {
+            meshFilter.mesh = block.GenerateMesh();
+            
+            var texture = Render3D.Instance.Snapshot(blockGO);
+            var slot = slots.AtIndex(slotIndex);
+            var image = new Image();
+            image.image = texture; 
+            if (slot.childCount > 0)
+                slot.RemoveAt(0);
+            slot.Add(image);
+        }
+        SetActiveToolbarItem(0);
+    }
+
+    // Called from Dashboard
+    public void SetToolbarItem(int slotIndex, Texture blockImage)
+    {
+        slotIndex = slotIndex - 1;
+        if (slotIndex == -1)
+            slotIndex = 9;
+
+        var slot = slots.AtIndex(slotIndex);
+        var image = new Image();
+        image.image = blockImage;
+        if (slot.childCount > 0)
+            slot.RemoveAt(0);
+        slot.Add(image);
+    }
+
+    public void SetActiveToolbarItem(int slotIndex)
+    {
+        selectedItem = slotIndex;
+        for (var i = 0; i < 10; i++)
+            slots.AtIndex(i).RemoveFromClassList("toolbarSlotActive");
+        slots.AtIndex(slotIndex).AddToClassList("toolbarSlotActive");
+    }
+}

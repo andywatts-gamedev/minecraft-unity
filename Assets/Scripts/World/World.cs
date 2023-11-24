11b27555160c7174cfe8f5661aac5bc00005d0f9
Andrew Watts-Curnow <andywatts@yahoo.com>
2023-11-23 22:39:15 -0500

    World using Mesher class

--- Assets/Scripts/World/World.cs
+++ Assets/Scripts/World/World.cs
@@ -52,14 +52,12 @@ public class World : MonoBehaviour
         _instance = this;
         buildingSiteDebugGOs = new List<GameObject>();
         meshCollider = GetComponent<MeshCollider>();
-
     }
     
     private void Start()
     {
         ComputeVoxels();
         UpdateMesh();
-        
         CreateBobs();
         GetComponent<MeshRenderer>().materials[0].SetTexture("_TextureArray", Textures.Instance.opaqueTexture2DArray);
         // GetComponent<MeshRenderer>().materials[1].SetTexture("_TextureArray", Textures.Instance.alphaClipTexture2DArray);
@@ -71,13 +69,12 @@ public class World : MonoBehaviour
         DebugBuildingSites();
     }
 
-
     public void UpdateMesh()
     {
-        ComputeFaces();
-        ComputeMesh();
-        // GetComponent<NavMeshSurface>().BuildNavMesh();
+        mesh = Mesher.Compute(dims, voxels);
+        GetComponent<MeshFilter>().mesh = mesh;
         ComputeHeightMap();    // TODO still used?
+        // GetComponent<NavMeshSurface>().BuildNavMesh();
         // ComputeBuildingSites();  // TODO What to do about Bobs enroute?
     }
     
@@ -138,205 +135,6 @@ public class World : MonoBehaviour
         }
     }
 
-    private void ComputeFaces()
-    {
-        // Debug.Log("Compute Faces");
-        var numFaces = dims.x * dims.y * dims.z * 6;
-        faces = new NativeArray<Face>(numFaces, Allocator.Temp);
-        for (var i = 0; i < numFaces; i++)
-        {
-            var side = i % 6;
-            var voxelIndex = i / 6; // Calculate the voxel index from the job index
-            var voxelXyz = voxelIndex.ToInt3(dims);
-            var voxel = voxels[voxelIndex];
-            // Debug.Log($"voxel {voxelXyz} = {voxels[voxelIndex]}");
-
-            var adjacentVoxelXyz = voxelXyz + Offsets[side];
-            var adjacentVoxelIndex = adjacentVoxelXyz.ToIndex(dims);
-            var adjacentIsWithinBounds = IsWithinBounds(adjacentVoxelXyz, dims);
-            var adjacentVoxel = (ushort)(adjacentIsWithinBounds ? voxels[adjacentVoxelIndex] : 0);
-            // Debug.Log($"adjacentVoxelXyz {adjacentVoxelXyz} = {voxels[voxelIndex]}");
-
-            if (IsFaceVisible(Blocks.Instance.blocks[voxel].Type, Blocks.Instance.blocks[adjacentVoxel].Type))
-            {
-                // Debug.Log($"Got face {side} for voxel {voxel} at {voxelXyz}  {(byte)Blocks.Instance.blocks[voxel].SideTextures[side].TextureObject.TextureIndex}");
-                var textureObject = Blocks.Instance.blocks[voxel].SideTextures[side].TextureObject;
-                faces[i] = new Face
-                {
-                    TextureIndex = (ushort)textureObject.TextureIndex,
-                    BlockType    = Blocks.Instance.blocks[voxel].Type,
-                    // BlockLight   = adjacentIsWithinBounds ? blockLights[adjacentVoxelIndex] : blockLightDefault,
-                    // SkyLight     = adjacentIsWithinBounds ? skyLights[adjacentVoxelIndex] : skyLightDefault,
-                    BlockLight   = 0,
-                    SkyLight     = 0,
-                    Metallic     = adjacentIsWithinBounds ? textureObject.Metallic : (byte)0,
-                    Smoothness   = adjacentIsWithinBounds ? textureObject.Smoothness : (byte)0,
-                };
-            }
-        }
-        // Debug.Log($"Got {faces.Count(f => !f.Equals(default(Face)))} faces");
-    }
-
-    private void ComputeMesh()
-    { 
-        // Define MeshData
-        var numFaces = faces.Count(f => !f.Equals(default(Face)));
-        var meshDataArray = Mesh.AllocateWritableMeshData(1);
-        var meshData = meshDataArray[0];
-        meshData.SetVertexBufferParams(numFaces * 4, new[]
-        {
-            new VertexAttributeDescriptor( VertexAttribute.Position, dimension: 3, stream: 0),
-            new VertexAttributeDescriptor( VertexAttribute.Normal, dimension: 3, stream: 0 ),
-            new VertexAttributeDescriptor( VertexAttribute.Tangent, VertexAttributeFormat.Float16, dimension: 4, stream: 0 ),
-            new VertexAttributeDescriptor( VertexAttribute.TexCoord0, VertexAttributeFormat.Float16, dimension: 2, stream: 1 ),
-            new VertexAttributeDescriptor( VertexAttribute.Color, VertexAttributeFormat.UNorm8, dimension: 4, stream: 2 ),
-        });
-        meshData.SetIndexBufferParams(numFaces * 6, IndexFormat.UInt16);
-        
-        
-        // Populate MeshData
-        var vertexStream0 = meshData.GetVertexData<VertexStream0>(0);
-        var triangles = meshData.GetIndexData<ushort>();
-        var texCoords = meshData.GetVertexData<half2>(1);
-        var colors = meshData.GetVertexData<Color32>(2);
-
-
-        var opaqueTriangles = new NativeList<ushort>(Allocator.Temp);
-        var transparentTriangles = new NativeList<ushort>(Allocator.Temp);
-        var alphaClipTriangles = new NativeList<ushort>(Allocator.Temp);
-        NativeList<ushort> tris;
-        
-        var faceCount = 0;
-        for (var i = 0; i < faces.Length; i++)
-        {
-            if (faces[i].Equals(default(Face)))
-                continue;
-
-            // Triangles
-            if (faces[i].BlockType == BlockType.AlphaClip)
-                tris = alphaClipTriangles;
-            else if (faces[i].BlockType == BlockType.Transparent)
-                tris = transparentTriangles;
-            else 
-                tris = opaqueTriangles;
-            tris.Add((ushort)(faceCount*4 + 0));
-            tris.Add((ushort)(faceCount*4 + 1));
-            tris.Add((ushort)(faceCount*4 + 2));
-            tris.Add((ushort)(faceCount*4 + 2));
-            tris.Add((ushort)(faceCount*4 + 3));
-            tris.Add((ushort)(faceCount*4 + 0));
-                
-            // Vertex
-            var voxelIndex = i / 6;
-            var side = (Side) (i % 6);
-            var voxelXyz = voxelIndex.ToInt3(dims);
-
-            if (side == Side.East)
-            {
-                vertexStream0[faceCount*4 + 0] = new VertexStream0 {Position = new float3(1f, 0f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 1] = new VertexStream0 {Position = new float3(1f, 1f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 2] = new VertexStream0 {Position = new float3(1f, 1f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 3] = new VertexStream0 {Position = new float3(1f, 0f, 1f) + voxelXyz};
-            }
-            else if (side == Side.Up)
-            {
-                vertexStream0[faceCount*4 + 0] = new VertexStream0 {Position = new float3(0f, 1f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 1] = new VertexStream0 {Position = new float3(0f, 1f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 2] = new VertexStream0 {Position = new float3(1f, 1f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 3] = new VertexStream0 {Position = new float3(1f, 1f, 0f) + voxelXyz};
-            }
-            else if (side == Side.North)
-            {
-                vertexStream0[faceCount*4 + 0] = new VertexStream0 {Position = new float3(1f, 0f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 1] = new VertexStream0 {Position = new float3(1f, 1f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 2] = new VertexStream0 {Position = new float3(0f, 1f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 3] = new VertexStream0 {Position = new float3(0f, 0f, 1f) + voxelXyz};
-            }
-            else if (side == Side.West)
-            {
-                vertexStream0[faceCount*4 + 0] = new VertexStream0 {Position = new float3(0f, 0f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 1] = new VertexStream0 {Position = new float3(0f, 1f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 2] = new VertexStream0 {Position = new float3(0f, 1f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 3] = new VertexStream0 {Position = new float3(0f, 0f, 0f) + voxelXyz};
-            }
-            else if (side == Side.Down)
-            {
-                vertexStream0[faceCount*4 + 0] = new VertexStream0 {Position = new float3(0f, 0f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 1] = new VertexStream0 {Position = new float3(1f, 0f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 2] = new VertexStream0 {Position = new float3(1f, 0f, 1f) + voxelXyz};
-                vertexStream0[faceCount*4 + 3] = new VertexStream0 {Position = new float3(0f, 0f, 1f) + voxelXyz};
-            }
-            else if (side == Side.South)
-            {
-                vertexStream0[faceCount*4 + 0] = new VertexStream0 {Position = new float3(0f, 0f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 1] = new VertexStream0 {Position = new float3(0f, 1f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 2] = new VertexStream0 {Position = new float3(1f, 1f, 0f) + voxelXyz};
-                vertexStream0[faceCount*4 + 3] = new VertexStream0 {Position = new float3(1f, 0f, 0f) + voxelXyz};
-            }
-            
-            
-            // Texcoords
-            texCoords[faceCount*4 + 0] = (half)0;
-            texCoords[faceCount*4 + 1] = half2((half)0, (half)1);
-            texCoords[faceCount*4 + 2] = (half)1;
-            texCoords[faceCount*4 + 3] = half2((half)1, (half)0);
-            
-            // Colors
-            // TODO consider side and blockType to get textureIndex
-            var textureIndex = (byte)faces[i].TextureIndex;   // Must * 256 in SG to get index
-            var blockLight = faces[i].BlockLight;
-            var skyLight = faces[i].SkyLight;
-            var metallicSmoothness = PackValues(faces[i].Metallic, faces[i].Smoothness);
-            colors[faceCount * 4 + 0] = new Color32(blockLight, skyLight, metallicSmoothness,textureIndex);
-            colors[faceCount * 4 + 1] = new Color32(blockLight, skyLight, metallicSmoothness,textureIndex);
-            colors[faceCount * 4 + 2] = new Color32(blockLight, skyLight, metallicSmoothness,textureIndex);
-            colors[faceCount * 4 + 3] = new Color32(blockLight, skyLight, metallicSmoothness,textureIndex); 
-            
-            faceCount++;
-        }
-        
-        // Triangles
-        for (var i = 0; i < opaqueTriangles.Length; i++)
-            triangles[i] = opaqueTriangles[i];
-        for (var i = 0; i < alphaClipTriangles.Length; i++)
-            triangles[opaqueTriangles.Length +  i] = alphaClipTriangles[i];
-        for (var i = 0; i < transparentTriangles.Length; i++)
-            triangles[opaqueTriangles.Length + alphaClipTriangles.Length + i] = transparentTriangles[i];
-
-        meshData.subMeshCount = 3;
-        meshData.SetSubMesh(0, new SubMeshDescriptor(0, opaqueTriangles.Length));
-        meshData.SetSubMesh(1, new SubMeshDescriptor(opaqueTriangles.Length, alphaClipTriangles.Length));
-        meshData.SetSubMesh(2, new SubMeshDescriptor(opaqueTriangles.Length + alphaClipTriangles.Length, transparentTriangles.Length));
-
-        
-        mesh = new Mesh();
-        mesh.name = "ChunkMesh";
-        Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
-        GetComponent<MeshFilter>().mesh = mesh;
-        
-        meshCollider.sharedMesh = mesh;
-    }
-
-    private bool IsWithinBounds(int3 voxel, int3 dimensions)
-    {
-        return voxel.x >= 0 && voxel.x < dimensions.x &&
-               voxel.y >= 0 && voxel.y < dimensions.y &&
-               voxel.z >= 0 && voxel.z < dimensions.z;
-    }
-
-    private bool IsFaceVisible(BlockType blockType, BlockType otherBlockType)
-    {
-        return blockType switch
-        {
-            BlockType.Opaque => otherBlockType != BlockType.Opaque,
-            BlockType.AlphaClip => otherBlockType != BlockType.Opaque,
-            BlockType.Transparent => otherBlockType != BlockType.Opaque && otherBlockType != BlockType.Transparent,
-            _ => false
-        };
-    }
-
     void ComputeBuildingSites()
     {
         buildingSites = new List<Vector3>();
@@ -388,29 +186,6 @@ public class World : MonoBehaviour
     private void OnDestroy()
     {
         voxels.Dispose();
-    }
-
-    // --- Static Helpers ---
-    private static readonly int3[] Offsets =
-    {
-        new(1, 0, 0), // east
-        new(0, 1, 0), // up
-        new(0, 0, 1), // north
-        new(-1, 0, 0), // west
-        new(0, -1, 0), // down
-        new(0, 0, -1) // south
-    };
-
-    static byte PackValues(byte value1, byte value2)
-    {
-        // Ensure that values are within the 4-bit range
-        value1 = (byte)(value1 & 0x0F);
-        value2 = (byte)(value2 & 0x0F);
-        
-        // Pack the values into a single byte
-        byte packedValue = (byte)((value1 << 4) | value2);
-
-        return packedValue;
     }
 
 }
